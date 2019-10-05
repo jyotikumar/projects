55dc8b578c86d99b0fc187fb7c66b2de28a456d0
Jyoti Kumar <jkuma14@gmail.com>
2019-10-05 10:52:24 +0530

    Created tree package and tree Node

--- src/datastructures/linkedlist/RecursiveLList.java
+++ src/datastructures/linkedlist/RecursiveLList.java
@@ -1,4 +1,4 @@
-package datastructures;
+package datastructures.linkedlist;
 
 public class RecursiveLList {
     Node head;
---
+++ src/datastructures/tree/Node.java
@@ -0,0 +1,13 @@
+package datastructures.tree;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
